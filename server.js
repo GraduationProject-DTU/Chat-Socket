d557c2197a4123e8a9679a80603b306f400eba47
chimanh <chimanh@techzen.vn>
2024-03-12 09:07:53 +0700

    hehe

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const socketio = require('socket.io');
 
-// Tạo server HTTP
 const server = http.createServer((req, res) => {
   fs.readFile(__dirname + '/index.html', (err, data) => {
     res.writeHead(200);
@@ -10,26 +9,22 @@ const server = http.createServer((req, res) => {
   });
 });
 
-// Kết nối Socket.io với server HTTP
 const io = socketio(server);
 
-// Xử lý các sự kiện kết nối và chat
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   // Lắng nghe sự kiện chat
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg); // Gửi lại tin nhắn cho tất cả các kết nối
+    io.emit('chat message', msg);
   });
 
-  // Lắng nghe sự kiện ngắt kết nối
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-// Khởi động server và lắng nghe trên cổng 3000
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
